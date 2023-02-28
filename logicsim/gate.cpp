2299ed9436e6e9c46b90d937cb9a12b601ee733e
Adam Raslan <araslan@usc.edu>
2023-02-28 23:20:56 +0000

    First grade for hw1

--- logicsim/gate.cpp
+++ logicsim/gate.cpp
@@ -21,6 +21,38 @@ void Gate::wireInput(unsigned int id, Wire* in)
     }
 }
 
+NotGate::NotGate(Wire* a, Wire*b, Wire* o) : Gate(2,o)
+{
+	wireInput(0, a);
+	wireInput(1, b); 
+}
+
+Event* NotGate::update(uint64_t current_time)
+{
+	char state;
+  Event* e = nullptr;
+	for(auto w : m_inputs)
+	{
+		char in = w->getState();
+		if(in == '0')
+		{
+			state = '1';
+			break;
+		}else if (in == '1'){
+      state = '0';
+      break;
+    }
+	}
+    if(state != m_current_state)
+	{
+    m_current_state = state;
+    uint64_t next = current_time + m_delay;
+		e = new Event {next,m_output,state}; 
+	}
+  return e;
+}
+
+
 And2Gate::And2Gate(Wire* a, Wire* b, Wire* o) : Gate(2,o)
 {
     wireInput(0,a);
